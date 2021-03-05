99c4a77837acaf534e189253d0a324188717d07a
Harshvardhan Dewangan <harshvardhandewangan@gmail.com>
2021-03-05 17:15:38 -0600

    firstcommit

---
+++ JS/taskscheduler.js
@@ -0,0 +1,84 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    
+}
+
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+    
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+    
+}
+
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function closeCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+
+    $('#vehicle1').change(function() {
+        if($(this).is(":checked")) {
+           $('.label1').css({"opacity":"0.5"});
+        } else {
+          $('.label1').css({"opacity":"1"});
+        }
+           
+    });
+});
+
+$(document).ready(function(){
+  var date_input=$('input[name="date"]'); //our date input has the name "date"
+  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  var options={
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
+})
+
