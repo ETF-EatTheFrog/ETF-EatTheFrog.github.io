cb46392d531218b89099e837325358a38bde9edf
Harshvardhan Dewangan <harshvardhandewangan@gmail.com>
2021-05-01 12:17:08 -0500

    login function

--- login.js
+++ login.js
@@ -1,2 +1,16 @@
 // var username  = $('#username').attr('value');
 // window.localStorage.setItem('username',username);
+
+var modal = document.getElementById("myModal");
+modal.style.display = "none";
+
+$("#LoginButton").click(function() {
+    if($("#name2").val() == "etf@gmail.com" && $("#pass2").val() == "frog") {
+        window.location.replace("https://etf-eatthefrog.github.io/index.html");
+        return false;
+    
+    } else {
+        alert("Username & Password is incorrect");
+    }
+});
+
